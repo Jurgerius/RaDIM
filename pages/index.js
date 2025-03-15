5c315a3d2b10b4e40fde29340f8280c167043d1e
Juraj Mišejka <juraj@Mac-mini.local>
2025-03-15 22:10:50 +0100

    Opraveno přesměrování na index.html

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    window.location.href = "/index.html"; // Přesměrování na původní HTML
+    window.location.replace("/index.html"); // Přesměrování na původní statickou stránku
   }, []);
 
   return <p>Přesměrování...</p>;
