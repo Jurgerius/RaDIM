a5f63a02dd4f0663fe6ac3540324bf6b2246990e
Juraj Mišejka <juraj@Mac-mini.local>
2025-03-16 16:58:48 +0100

    Debug: Test Vercel Analytics

--- app/app.js
+++ app/app.js
@@ -3,14 +3,17 @@ import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   useEffect(() => {
+    console.log("✅ useEffect byl spuštěn.");
+    console.log("🌐 Aktuální URL:", window.location.href);
+    alert("🔍 Vercel Analytics aktivní?"); // Zobrazí popup okno
     console.log("🔍 Vercel Analytics aktivní?");
-    window.location.replace("/index.html"); // Přesměrování na původní statickou stránku
+    window.location.replace("/index.html"); 
   }, []);
 
   return (
     <>
       <p>Přesměrování...</p>
-      <Analytics /> {/* Přidání Vercel Analytics */}
+      <Analytics />
     </>
   );
 }
