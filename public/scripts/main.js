4f483fb6fa9b858befc468f388751e0bb529dbe7
Juraj Mišejka <juraj@Mac-mini.local>
2025-03-16 19:08:51 +0100

    Aktualizace kódu a opravy

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -84,23 +84,3 @@ window.toggleMenu = function() {
 
 
 window.addEventListener("scroll", handleScroll);
-
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("✅ app.js byl načten správně.");
-
-  // Přidání Vercel Analytics skriptu do stránky
-  var script = document.createElement("script");
-  script.src = "https://vercel.live/_analytics/script.js";
-  script.async = true;
-
-  script.onload = function () {
-    console.log("✅ Vercel Analytics se načetl!");
-    console.log("📊 Kontrola dostupnosti:", window.__VERCEL_ANALYTICS__);
-  };
-
-  script.onerror = function () {
-    console.error("❌ Chyba při načítání Vercel Analytics!");
-  };
-
-  document.head.appendChild(script);
-});
