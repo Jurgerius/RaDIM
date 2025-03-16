3533e4a7b017cac9a57da0ab4b1a2565afc18c0c
Juraj Mišejka <juraj@Mac-mini.local>
2025-03-16 12:22:12 +0100

    Aktualizace projektu: změny v .gitignore, app.js, package.json

--- app/app.js
+++ app/app.js
@@ -1,9 +1,15 @@
 import { useEffect } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   useEffect(() => {
     window.location.replace("/index.html"); // Přesměrování na původní statickou stránku
   }, []);
 
-  return <p>Přesměrování...</p>;
+  return (
+    <>
+      <p>Přesměrování...</p>
+      <Analytics /> {/* Přidání Vercel Analytics */}
+    </>
+  );
 }
